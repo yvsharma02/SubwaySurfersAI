f6efc20395198190baa224aed0e985f02bd65e7b
yvsharma02 <yvsharma02@gmail.com>
2024-08-14 23:14:54 +0530

    More epxeriments

--- architecture.py
+++ architecture.py
@@ -5,26 +5,30 @@ def get_model() -> models.Model:
     model = models.Sequential()
     model.add(layers.Input(shape=config.TRAINNIG_DATA_DIMENSIONS))
     model.add(layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu')))
-    model.add(layers.TimeDistributed(layers.AveragePooling2D((3, 3))))
+    model.add(layers.TimeDistributed(layers.AveragePooling2D((2, 2))))
     model.add(layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu')))
     model.add(layers.TimeDistributed(layers.AveragePooling2D((2, 2))))
     model.add(layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu')))
     model.add(layers.TimeDistributed(layers.Flatten()))
-    model.add(layers.TimeDistributed(layers.Dropout(0.15)))
-    
+    model.add(layers.TimeDistributed(layers.Dropout(0.2)))
     model.add(
-        layers.TimeDistributed(layers.Dense(300, activation='relu'))
+        layers.TimeDistributed(layers.Dense(200, activation='relu'))
     )
-    model.add(layers.Dropout(0.15))
+    model.add(layers.Dropout(0.125))
     model.add(
-        layers.LSTM(50, activation='relu', return_sequences=False)
+        layers.TimeDistributed(layers.Dense(200, activation='relu'))
+    )
+    model.add(layers.Dropout(0.125))
+    model.add(
+        layers.LSTM(50, activation='tanh', return_sequences=False)
     )
     model.add(
-        layers.Dense(50, activation='relu')
+        layers.Dense(20, activation='relu')
     )
     model.add(
-        layers.Dense(50, activation='tanh')
+        layers.Dense(20, activation='relu')
     )
+
     model.add(layers.Dense(5))
     model.add(layers.Softmax())
 
